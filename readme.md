b8b2e77c0e8a8137bbaf796cad78b43b944e8fec
rajuboyella1753 <rajuboyella737@gmail.com>
2024-08-24 18:25:05 +0530

    New file

---
+++ readme.md
@@ -0,0 +1,8 @@
+#  Project 27 | Color Picker
+![image](./images/logo3.png)
+
+
+|Name|Date|Project|
+|---|---|---|
+|Raju|24/08/2024|27|
+
---
+++ script.js
@@ -0,0 +1,22 @@
+let message = document.getElementById("message");
+function first(){
+  document.body.style.backgroundColor = "#e0e0e0";
+  message.textContent = "#e0e0e0";
+  message.style.color = "#e0e0e0";
+}
+function second(){
+  document.body.style.backgroundColor = "#6fcf97";
+  message.textContent = "#6fcf97";
+  message.style.color = "#6fcf97";
+}
+function third(){
+  document.body.style.backgroundColor = "#56ccf2";
+  message.textContent = "#56ccf2";
+  message.style.color = "#56ccf2";
+}
+function fourth(){
+  document.body.style.backgroundColor = "#bb6bd9";
+  message.textContent = "#bb6bd9";
+  message.style.color = "#bb6bd9";
+}
+
